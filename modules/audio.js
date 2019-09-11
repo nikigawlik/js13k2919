0e6eebea4b98a96d8c424f0bd3dd3ca78bf919b6
Nils Gawlik <nilsgawlik@gmx.de>
2019-09-11 12:50:28 +0200

    upgrade the music with glitchy sounds

--- modules/audio.js
+++ modules/audio.js
@@ -19,6 +19,40 @@ export default class Audio {
         let data = musicBuffer.getChannelData(0);
 
         let flavor;
+        
+        let b = 0;
+        for(let i = 0; i < 5; i++) {
+            b = b | ~~(random() * 4);
+            b = b << 2;
+        }
+        // look at those magic numbers!
+        let bs = [
+            773752, // very fast upbeat
+            773752, // very fast upbeat
+            773752|0x7f, // very fast upbeat
+            957758312, // fast ish
+
+            773752, // very fast upbeat
+            773752, // very fast upbeat
+            773752|0x7f, // very fast upbeat
+            957758312, // fast ish
+
+            792323628, // noisy? slow
+            792323628, // noisy? slow
+            957758312, // fast ish
+            957758312, // fast ish
+
+            1008437436, // fast ish synth
+            1008437436, // fast ish synth
+            773752|0x7f, // very fast upbeat
+            957758312, // fast ish
+
+            // -409420504, // interesting medium
+            // 792323628,// noisy? slow
+            // 345849296, // continuus subbase
+            // 1962361744, // fast tension
+        ];
+        // let bs = [345849296,1962361744,957758312,792323628,773752,1008437436,773752,792323628,773752,792323628];
 
         for(let t = 0; t < length; t++) {
             const env = 1-(t % (1<<12) / (1<<12));
@@ -29,15 +63,29 @@ export default class Audio {
             let kick = sin(env**6*0x6f)*env;
             let snare = (random()*2-1)*env**4;
             if(t % (1<<15) == 0) flavor = ~~(random() * 2048);
-            data[t] = 
+            if(t % (1<<16) == 0) {
+                // this is how magic numbers are originally made!
+                // for(let i = 0; i < 5; i++) {
+                //     b = b | ~~(random() * 4);
+                //     b = b << 2;
+                // }
+                // bs.push(b);
+                b = bs.shift();
+            };
+            let tt = t;
+            t = t & b;
+            data[tt] = 
                 [[kick,hihat1,snare,hihat1][(t>>12)%4] * 0.4, [kick,snare,hihat1,hihat2][(t>>12)%4] * 0.4][max((t>>14)%4-2, 0)]
-                + sign(sin((t&((1<<(5+(t>>12)%6))-1))*(1/64)*PI*(1+env2*.25))) * 0.04
-                + sign(sin((t&(flavor << 4))*(1/64)*PI)) * 0.04
+                + sign(sin((t&((1<<(5-(t>>14)%3+(t>>12)%6))-1))*(1/64)*PI*(1+env2*.25))) * 0.05
+                + sign(sin((t&(flavor << 4))*(1/64)*PI)) * 0.05
                 // + sign(sin((t&([32,2,3,111][(t>>15)%4] << 4))*(1/64)*PI)) * 0.1
                 + data[max(0, t - 2100)] * 0.4
                 + data[max(0, t - 2000)] * 0.4
             ;
+            t = tt;
         }
+        
+        console.log(bs.toString());
 
         let musicSource = this.ctx.createBufferSource();
         musicSource.buffer = musicBuffer;
